abe9e9161bc20edf68a7919fb3b3cf8d0e887e44
cack195 <cackamr195@gmail.com>
2023-04-08 15:05:17 +0530

    navbar

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>hello</h1>
+    <>
+    <nav className='navbar'>
+      <h1>
+        Listify
+      </h1>
+    </nav>
+    </>
   )
 }
 
